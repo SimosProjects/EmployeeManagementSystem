1b7f6bfc91b0411cca28798cffed9a1d5e9b0bea
Chris <Chris@LINK>
2024-01-30 18:59:24 -0800

    Add project files.

---
+++ Views/RegistrationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace EmployeeManagementSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for RegistrationView.xaml
+    /// </summary>
+    public partial class RegistrationView : Window
+    {
+        public RegistrationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
