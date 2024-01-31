e37ede9389dad4accd012c21b3ea15440fa71695
Christopher Simonson <simonsonchristopher@gmail.com>
2024-01-30 20:12:31 -0800

    Fixed formatting, started AddEmployeeView

---
+++ Views/AddEmployeeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EmployeeManagementSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for AddEmployeeView.xaml
+    /// </summary>
+    public partial class AddEmployeeView : UserControl
+    {
+        public AddEmployeeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
